cf9b8ba876fb0555b0dbe49a352ceb20dcf074bf
GitHub Action <action@github.com>
2024-07-26 16:35:53 +0000

    Releasing version 3.5.14 - Xcode 14.3

--- Example/Example/MainView.swift
+++ Example/Example/MainView.swift
@@ -8,98 +8,55 @@
 import SwiftUI
 
 import AtomicTransact
-import AtomicTransactSwiftUI
-
-
-class ConfigData: ObservableObject {
-	@Published var showTransact = false
-	var productType = AtomicConfig.ProductType.deposit
-	var scopeType = AtomicConfig.ScopeType.userLink
-	
-	func show(product: AtomicConfig.ProductType, scope: AtomicConfig.ScopeType) {
-		self.productType = product
-		self.scopeType = scope
-		self.showTransact = true
-	}
-}
 
 
 struct MainView: View {
 	@ObservedObject var data: TransactData
-	@StateObject var config = ConfigData()
-	
-	var switchToSettings: (() -> ())?
+	@State var productType = AtomicConfig.ProductType.deposit
+	@State var showingTransact = false
 	
 	var body: some View {
 		NavigationView {
 			Form {
-				if data.publicToken == "" {
-					VStack(alignment: .center, spacing: 8) {
-						HStack {
-							Image(systemName: "exclamationmark.triangle")
-								.foregroundColor(Color.red)
-							Text("Don't forget to set your public token!")
-						}
-						
-						Button {
-							switchToSettings?()
-						} label: {
-							HStack {
-								Text("Add it now")
-								Image(systemName: "arrow.right")
-								Image(systemName: "slider.horizontal.below.square.filled.and.square")
-							}
-						}
-					}
-					.multilineTextAlignment(.center)
-					.frame(maxWidth: .infinity, alignment: .center)
-					.listRowBackground(Color.clear)
-					.font(.subheadline)
-				}
-				
 				Section {
-					Button {
-						config.show(product: .deposit, scope: .userLink)
-					} label: {
-						Label("Setup direct deposit", systemImage: "pencil.and.ellipsis.rectangle")
+					Button("Setup direct deposit") {
+						self.productType = .deposit
+						self.showingTransact = true
 					}
 				}
 				
 				Section {
-					Button {
-						config.show(product: .verify, scope: .userLink)
-					} label: {
-						Label("Verify employment status", systemImage: "person.crop.circle.badge.checkmark")
+					Button("Verify employment status") {
+						self.productType = .verify
+						self.showingTransact = true
 					}
 				}
 				
 				Section {
-					Button {
-						config.show(product: .switch, scope: .payLink)
-					} label: {
-						Label("Switch payment method", systemImage: "creditcard.and.123")
+					Button("Confirm identity") {
+						self.productType = .identify
+						self.showingTransact = true
 					}
 				}
 			}
 			.navigationTitle("Home")
 		}
 		.atomicTransact(
-			isPresented: $config.showTransact,
+			isPresented: $showingTransact,
 			config: {
 				AtomicConfig(
 					publicToken: data.publicToken,
-					scope: config.scopeType,
-					tasks: [.init(product: config.productType)],
-					theme: data.theme)
+					product: productType,
+					theme: .init(brandColor: UIColor(data.brandColor), overlayColor: UIColor(data.overlayColor)))
 			},
-			environment: data.transactURL,
+			environment: data.environment,
 			presentsFullscreen: data.showFullscreen)
 	}
 }
 
 
 struct MainView_Previews: PreviewProvider {
-	static var previews: some View {
-		MainView(data: TransactData())
-	}
+    static var previews: some View {
+        MainView(data: TransactData())
+    }
 }
--- Example/Example/TransactData.swift
+++ Example/Example/TransactData.swift
@@ -13,43 +13,24 @@ import AtomicTransact
 class TransactData: ObservableObject {
 	enum URLOption: String, Identifiable, CaseIterable {
 		case production
+		case sandbox
 		case custom
 		
 		var id: Self { self }
 	}
 	
-	enum DarkModeOption: String, Identifiable, CaseIterable {
-		case system
-		case dark
-		case light
-		
-		var id: Self { self }
-	}
-	
-	@AppStorage("URLOption") var urlOption = URLOption.production
+	@AppStorage("URLOption") var urlOption = URLOption.sandbox
 	@AppStorage("customURLPath") var customURLPath = ""
 	@AppStorage("showFullscreen") var showFullscreen = false
 	@AppStorage("PublicToken") var publicToken = ""
-	@AppStorage("DarkMode") var darkMode = DarkModeOption.system
 	@Published var brandColor = Color.blue
 	@Published var overlayColor = Color.gray
-	@Published var demoModeColor = Color.green
 	
-	var transactURL: TransactEnvironment {
+	var environment: TransactEnvironment {
 		switch urlOption {
 		case .production: return .production
+		case .sandbox: return .sandbox
 		case .custom: return .custom(path: customURLPath)
-		}
-	}
-	
-	var theme: AtomicConfig.Theme {
-		switch darkMode {
-		case .system:
-			return .init(brandColor: UIColor(brandColor), overlayColor: UIColor(overlayColor), dark: .system)
-		case .dark:
-			return .init(brandColor: UIColor(brandColor), overlayColor: UIColor(overlayColor), dark: .dark)
-		case .light:
-			return .init(brandColor: UIColor(brandColor), overlayColor: UIColor(overlayColor), dark: .light)
 		}
 	}
 }
